e5d15d67beb74c0c19e613c2af8ba0b747d64336
Yasin Kaya <yasinkaya.121@gmail.com>
2018-09-05 15:37:34 +0300

    changed library version on setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sixfab_cellulariot',
-    version='0.1',
+    version='1.0',
     author='Yasin Kaya',
     author_email='yasinkaya.121@gmail.com',
     description='sixfab linux libraries',
