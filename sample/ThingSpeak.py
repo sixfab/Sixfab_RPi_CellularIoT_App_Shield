0cfaf26f467a81ed7cebff95dd502d3e3278db3c
Metin KOC <saucompeng@gmail.com>
2018-09-11 16:06:14 +0300

    Added ThingSpeak Http Post example via AT Command.

---
+++ sample/ThingSpeak.py
@@ -0,0 +1,45 @@
+'''
+  ThingSpeak.py - This is send data to ThingSpeak via http post method.
+  Created by Metin KOC (saucompeng), September 11, 2018.
+'''
+from cellulariot import cellulariot
+import time
+
+api_key = "XXXXXXXXXXXXX"; # change with api-key
+
+data = "field1=%d"
+
+node = cellulariot.CellularIoTApp()
+node.disable()
+node.enable()
+node.powerUp()
+
+node.sendATComm("ATE1","OK\r\n")
+
+node.getIMEI()
+time.sleep(0.5)
+node.getFirmwareInfo()
+time.sleep(0.5)
+node.getHardwareInfo()
+time.sleep(0.5)
+
+node.setGSMBand(node.GSM_900)
+time.sleep(0.5)
+node.setMode(node.GSM_MODE)
+time.sleep(0.5)
+
+node.connectToOperator()
+time.sleep(0.5)
+node.getSignalQuality()
+time.sleep(0.5)
+node.getQueryNetworkInfo()
+time.sleep(0.5)
+
+node.deactivateContext()
+time.sleep(0.5)
+node.activateContext()
+time.sleep(0.5)
+
+node.sendDataThingspeak(api_key, data % node.readTemp())
+
+
