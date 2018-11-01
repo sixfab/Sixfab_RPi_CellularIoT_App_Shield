b3542a787a8e6d9bec1a9101e3e47ec46b708ce1
Yasin Kaya <yasinkaya.121@gmail.com>
2018-11-01 19:45:19 +0000

    changed setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sixfab_cellulariot',
-    version='1.0',
+    version='1.1',
     author='Yasin Kaya',
     author_email='yasinkaya.121@gmail.com',
     description='sixfab linux libraries',
