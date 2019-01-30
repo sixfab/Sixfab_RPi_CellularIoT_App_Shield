0b222182e8a9181bd03126b1adce9be43450d3ab
Yasin Kaya <yasinkaya.121@gmail.com>
2019-01-30 14:48:59 +0300

    updated version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='sixfab_cellulariot',
-    version='1.1',
+    version='1.2.0',
     author='Yasin Kaya',
     author_email='yasinkaya.121@gmail.com',
     description='sixfab linux libraries',
     license='MIT',
-    url='https://github.com/sixfab/Sixfab_RPi_CellularIoT_Library',
+    url='https://github.com/sixfab/Sixfab_RPi_CellularIoT_App_Shield',
     dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.9.3'],
 	install_requires  = ['Adafruit-GPIO>=0.9.3', 'pyserial', 'adafruit-ads1x15'],
     packages=find_packages()
